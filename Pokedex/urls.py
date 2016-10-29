af7097548ab6ff9213a520c2f6988743daa49466
marco <mrc5622@gmail.com>
2016-10-29 22:39:20 +0000

    Primeras APIS creadas con RestFramework sobre las tablas User y Groups.

--- Pokedex/urls.py
+++ Pokedex/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from apis import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSer)
 
 urlpatterns = [
+	url(r'^', include(router.urls)),
 	url(r'^pokemon/', include('pokemon.urls')),
+	url(r'^api-auth/', include('rest_framework.urls', namespace = 'rest_framework' )),
     url(r'^admin/', admin.site.urls),
 ]
